0504caab1dd02f01be7932c4d6c87111ec4f0160
mesteban07 <53617939+mesteban07@users.noreply.github.com>
2019-09-13 09:19:18 -0500

    Update prueba2.py

--- prueba2.py
+++ prueba2.py
@@ -5,65 +5,81 @@ from collections import deque
 from imutils.video import VideoStream
 
 #cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture("video_luz_natural.mp4")
+#cap = cv2.VideoCapture("video_luz_natural.mp4")
+cap = cv2.VideoCapture("video_luz_natural_2.mp4")
 kernel = np.ones((5,5),np.uint8)
 
 # Buffer to draw tracking path
 bufferSize = 64
 pts = deque(maxlen=bufferSize)
-
+contador_rojo=0
+contador_azul=0
+contador_amarillo=0
+dim_x = cap.get(3)
 
 while(1):
+    #print("amarillos= ",contador_amarillo)
+    #print("rojos= ",contador_rojo)
+    #print("azules= ",contador_azul)
 
     # Take each frame
     _, frame = cap.read()
+
+    #dim_x, dim_y,_ = frame.shape
+    #print ("dim_x",dim_x,"dim_y",dim_y)
+    limite=int(dim_x/2)
 
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
     # define range of blue color in HSV
-    lower_blue = np.array([105,30,30])
+    lower_blue = np.array([105,30,20])
     upper_blue = np.array([135,255,255])
 
     # define range of red color in HSV
-    lower_red = np.array([0,80,72])
+    lower_red = np.array([0,180,75])
     upper_red = np.array([3,255,255])
-    lower_red_1 = np.array([177,80,72])
+
+    lower_red_1 = np.array([177,180,75])
     upper_red_1 = np.array([180,255,255])
 
     # define range of yellow color in HSV
-    lower_yellow = np.array([20,57,50])
+    lower_yellow = np.array([19,56,54])
     upper_yellow = np.array([29,255,255])
     
     # Threshold the HSV image to get only blue colors
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+    # close
+    mask_blue=cv2.morphologyEx(mask_blue, cv2.MORPH_CLOSE, kernel)
+	# Erode 
+    mask_blue = cv2.erode(mask_blue, None, iterations=10)
+    # open
+    mask_blue=cv2.morphologyEx(mask_blue, cv2.MORPH_OPEN, kernel)
+	# dilate 
+    mask_blue = cv2.dilate(mask_blue, None, iterations=8)
+
+
     mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
     mask_red = cv2.inRange(hsv, lower_red, upper_red)
     mask_red_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
 
 	# Erode 2 times
-    mask_blue = cv2.erode(mask_blue, None, iterations=2)
+    mask_yellow = cv2.erode(mask_yellow, None, iterations=8)
 
 	# dilate 2 times
-    mask_blue = cv2.dilate(mask_blue, None, iterations=2)
+    mask_yellow = cv2.dilate(mask_yellow, None, iterations=6)
 
 	# Erode 2 times
-    mask_yellow = cv2.erode(mask_yellow, None, iterations=2)
+    mask_red = cv2.dilate(mask_red, None, iterations=8)
 
 	# dilate 2 times
-    mask_yellow = cv2.dilate(mask_yellow, None, iterations=2)
+    mask_red = cv2.erode(mask_red, None, iterations=6)
 
 	# Erode 2 times
-    mask_red = cv2.erode(mask_red, None, iterations=2)
+    mask_red_1 = cv2.erode(mask_red_1, None, iterations=8)
 
 	# dilate 2 times
-    mask_red = cv2.dilate(mask_red, None, iterations=2)
-
-	# Erode 2 times
-    mask_red_1 = cv2.erode(mask_red_1, None, iterations=2)
-
-	# dilate 2 times
-    mask_red_1 = cv2.dilate(mask_red_1, None, iterations=2)
+    mask_red_1 = cv2.dilate(mask_red_1, None, iterations=6)
 
     
     res_blue = cv2.bitwise_and(frame,frame, mask= mask_blue)
@@ -71,6 +87,8 @@ while(1):
     res_red_1 = cv2.bitwise_and(frame,frame, mask= mask_red_1)
     res_red = cv2.bitwise_or(res_red,res_red_1)
     res_yellow = cv2.bitwise_and(frame,frame, mask= mask_yellow)
+    res = cv2.bitwise_or(res_yellow,res_blue)
+    res = cv2.bitwise_or(res,res_red)
 
 
     
@@ -82,44 +100,43 @@ while(1):
 
 
 
-    cnts,_= cv2.findContours(mask_red.copy(), cv2.RETR_EXTERNAL,	cv2.CHAIN_APPROX_SIMPLE)
-	#cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-    center = None
-    print(cnts)
+    cnts_red,_= cv2.findContours(mask_red.copy(), cv2.RETR_EXTERNAL,	cv2.CHAIN_APPROX_SIMPLE)
+    cnts_blue,_= cv2.findContours(mask_blue.copy(), cv2.RETR_EXTERNAL,	cv2.CHAIN_APPROX_SIMPLE)
+    cnts_yellow,_= cv2.findContours(mask_yellow.copy(), cv2.RETR_EXTERNAL,	cv2.CHAIN_APPROX_SIMPLE)
+    Canny=cv2.Canny(res, 70, 250)
+    cnts,_= cv2.findContours(Canny.copy(), cv2.RETR_EXTERNAL,	cv2.CHAIN_APPROX_SIMPLE)
 
-	# Check if contours were found
-    if len(cnts) > 0:
-        # Find the biggest area contour
-        c = max(cnts, key=cv2.contourArea)
-        # Extract the circle that encloses the contour
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        center = (int(x), int(y))
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(frame, (int(x), int(y)), 5, (0, 0, 255), -1)
+    for i in cnts:
+        # calculate moments of binary image
+        M = cv2.moments(i)
+        
 
-    # Add the current center to the queue
-    pts.appendleft(center)
+        # calculate x,y coordinate of center
+        try:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+        except ZeroDivisionError:
+            cX = 0
+            cY = 0
+
+        # put text and highlight the center
+        cv2.circle(frame, (cX, cY), 5, (255, 255, 255), -1)
+        cv2.putText(frame, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        #print ("cX=",cX)
+        """
+        if cX <= limite-10:# and cX >= limite+10:
+            print ("estoy en el limite")
+            if res_red[cX][cY] != 0:
+                print ("ROJO")
+            if res_blue[cX][cY] != 0:
+                print ("AZUL")
+            if res_yellow[cX][cY] != 0:
+                print ("AMARILLO")
+            """
+            #print ("prueba=",res[cX][cY])
+            #print ("azul",res)
+
     
-    # Loop over the queue
-    
-    for i in range(1, len(pts)):
-        # Ignore null points
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-            # Draw the line
-        thickness = int(np.sqrt(bufferSize / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-
-
-
-    # Aplicar las respectivas mascaras y obtener una combinacion de las mascaras resultantes
-    res = cv2.bitwise_and(frame,frame, mask= mask_blue)
-    res = cv2.Canny(res,	180,	200)
-
-
     cv2.imshow('frame',frame)
     #cv2.imshow('mask',mask)
     cv2.imshow('res',res)
@@ -128,3 +145,6 @@ while(1):
         break
 
 cv2.destroyAllWindows()
+print("amarillos= ",contador_amarillo)
+print("rojos= ",contador_rojo)
+print("azules= ",contador_azul)
