9d431ecdd024694baec626cb33e16618a942702e
mesteban07 <53617939+mesteban07@users.noreply.github.com>
2019-09-13 00:21:25 -0500

    Add files via upload

--- prueba2.py
+++ prueba2.py
@@ -24,23 +24,65 @@ while(1):
     # define range of blue color in HSV
     lower_blue = np.array([105,30,30])
     upper_blue = np.array([135,255,255])
+
+    # define range of red color in HSV
+    lower_red = np.array([0,80,72])
+    upper_red = np.array([3,255,255])
+    lower_red_1 = np.array([177,80,72])
+    upper_red_1 = np.array([180,255,255])
+
+    # define range of yellow color in HSV
+    lower_yellow = np.array([20,57,50])
+    upper_yellow = np.array([29,255,255])
     
     # Threshold the HSV image to get only blue colors
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    mask_red = cv2.inRange(hsv, lower_red, upper_red)
+    mask_red_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
 
 	# Erode 2 times
-    mask_blue = cv2.erode(mask_blue, None, iterations=4)
+    mask_blue = cv2.erode(mask_blue, None, iterations=2)
 
 	# dilate 2 times
-    #mask_blue = cv2.dilate(mask_blue, None, iterations=1)
+    mask_blue = cv2.dilate(mask_blue, None, iterations=2)
+
 	# Erode 2 times
-    #mask_blue = cv2.erode(mask_blue, None, iterations=4)
+    mask_yellow = cv2.erode(mask_yellow, None, iterations=2)
+
+	# dilate 2 times
+    mask_yellow = cv2.dilate(mask_yellow, None, iterations=2)
+
+	# Erode 2 times
+    mask_red = cv2.erode(mask_red, None, iterations=2)
+
+	# dilate 2 times
+    mask_red = cv2.dilate(mask_red, None, iterations=2)
+
+	# Erode 2 times
+    mask_red_1 = cv2.erode(mask_red_1, None, iterations=2)
+
+	# dilate 2 times
+    mask_red_1 = cv2.dilate(mask_red_1, None, iterations=2)
+
+    
+    res_blue = cv2.bitwise_and(frame,frame, mask= mask_blue)
+    res_red = cv2.bitwise_and(frame,frame, mask= mask_red)
+    res_red_1 = cv2.bitwise_and(frame,frame, mask= mask_red_1)
+    res_red = cv2.bitwise_or(res_red,res_red_1)
+    res_yellow = cv2.bitwise_and(frame,frame, mask= mask_yellow)
+
+
+    
 
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
 
 	# Get contours
-    cnts,_= cv2.findContours(mask_blue.copy(), cv2.RETR_EXTERNAL,	cv2.CHAIN_APPROX_SIMPLE)
+
+
+
+    cnts,_= cv2.findContours(mask_red.copy(), cv2.RETR_EXTERNAL,	cv2.CHAIN_APPROX_SIMPLE)
 	#cnts = cnts[0] if imutils.is_cv2() else cnts[1]
     center = None
     print(cnts)
@@ -75,7 +117,7 @@ while(1):
 
     # Aplicar las respectivas mascaras y obtener una combinacion de las mascaras resultantes
     res = cv2.bitwise_and(frame,frame, mask= mask_blue)
-
+    res = cv2.Canny(res,	180,	200)
 
 
     cv2.imshow('frame',frame)
